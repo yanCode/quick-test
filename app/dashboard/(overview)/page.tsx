850420d2fb586104ea383cb4f37ccb390407b4fb
Sheng-Yan, Zhang <yan.code@gmail.com>
2024-07-31 16:06:13 +0800

    feat: use stream

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,12 +1,14 @@
 import {lusitana} from "@/app/ui/fonts";
-import {fetchCardData, fetchLatestInvoices, fetchRevenue} from "@/app/lib/data";
+import {fetchCardData} from "@/app/lib/data";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import {Card} from "@/app/ui/dashboard/cards";
+import {Suspense} from "react";
+import {LatestInvoicesSkeleton, RevenueChartSkeleton} from "@/app/ui/skeletons";
 
 export default async function Page() {
-    const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
+
+
     const {
         numberOfInvoices,
         numberOfCustomers,
@@ -27,8 +29,12 @@ export default async function Page() {
         />
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-             <RevenueChart revenue={revenue}  />
-             <LatestInvoices latestInvoices={latestInvoices} />
+           <Suspense fallback={<RevenueChartSkeleton/>}>
+               <RevenueChart  />
+           </Suspense>
+             <Suspense fallback={<LatestInvoicesSkeleton/>}>
+                 <LatestInvoices />
+             </Suspense>
         </div>
 
     </main>;
