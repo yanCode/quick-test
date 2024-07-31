ad1ef5567b7f052f05658e502c305ec2f6d4a729
Sheng-Yan, Zhang <yan.code@gmail.com>
2024-07-31 08:39:55 +0800

    updates

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,6 @@
+import {CardSkeleton} from "@/app/ui/skeletons";
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <CardSkeleton />
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import {Inter, Lusitana, Poppins} from 'next/font/google';
+export const inter = Inter({
+    subsets:["latin"]
+})
+export const poppins = Poppins({
+    subsets:["latin"], weight:["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
+export const lusitana = Lusitana({
+    subsets:["latin"], weight:["400", "700"]
+})
